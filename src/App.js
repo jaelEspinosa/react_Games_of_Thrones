2136380ce2fcddc597ad26855be8f1be98738e60
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-04-06 16:42:54 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import Menu from './Components/Menu'
+import TestPrueba from "./Components/TestPrueba";
 function App() {
   return (
     <div className="App">
       <h1>GAME OF THRONES!!!</h1>
       <HomePage></HomePage>
       <Menu />
+      <TestPrueba />
     </div>
   );
 }
