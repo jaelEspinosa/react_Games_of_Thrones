60d6a109defd5bdd0b3f6dc2ba3151e74659e2c0
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-04-06 23:39:01 +0200

    add home and routing

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import HeaderFlags from "../Components/HeaderFlags";
+import Menu from "../Components/Menu";
+import './HomePage.scss'
 
 function HomePage() {
-  return <HeaderFlags></HeaderFlags>;
+  return <>
+  <div className="contain">
+  <HeaderFlags></HeaderFlags>;
+  <h1 className="title">JUEGO DE TRONOS</h1>
+  <Menu/>
+
+
+  </div>
+  
+
+  </>
+  
 }
 
 export default HomePage;
