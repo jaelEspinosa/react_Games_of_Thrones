7b3adec311d17a16f4a15fec0e3e4945861909a2
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-04-06 16:35:46 +0200

    add font got

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
-
+import Menu from './Components/Menu'
 function App() {
   return (
     <div className="App">
       <h1>GAME OF THRONES!!!</h1>
       <HomePage></HomePage>
+      <Menu />
     </div>
   );
 }
