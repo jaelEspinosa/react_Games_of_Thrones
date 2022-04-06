648b8b173577180fd0221fbe8d6df25ba4bc4085
Sergi Molina <sergimoli@gmail.com>
2022-04-06 23:44:38 +0200

    language stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import CharactersPage from "./Pages/CharactersPage";
 import CharactersDetailPage from "./Pages/CharactersDetailPage";
 import HousesPage from "./Pages/HousesPage";
 import HousesDetailPage from "./Pages/HousesDetailPage";
 import CronologyPage from "./Pages/CronologyPage";
+import { useTranslation } from "react-i18next";
 
 // import { Navigate, Outlet } from "react-router-dom";
 
 function App() {
+  const [isSpanish, setIsSpanish] = useState(true);
+  const { t, i18n } = useTranslation(["translation"]);
+
+  const changeLanguage = (code) => {
+    i18n.changeLanguage(code);
+  };
   return (
     <Router>
       <div className="App">
-        <h1>GAME OF THRONES!!!</h1>
-        <HomePage></HomePage>
+        <h1>{t("title")}</h1>
+
+        <div>
+          {/* <button type="button" onClick={() => changeLanguage("es")}>
+            {t("translation:es")}
+          </button>
+
+          <button type="button" onClick={() => changeLanguage("en")}>
+            {t("translation:en")}
+          </button> */}
+        </div>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/characters" element={<CharactersPage />} />
