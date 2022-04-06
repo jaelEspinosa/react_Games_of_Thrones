fa1db0d858e3390ed517b259191e65aba0e96e44
Sergi Molina <sergimoli@gmail.com>
2022-04-06 16:44:10 +0200

    test sergi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
-import Menu from './Components/Menu'
+import Menu from "./Components/Menu";
+import TestSergi from "./Components/TestSergi";
 function App() {
   return (
     <div className="App">
       <h1>GAME OF THRONES!!!</h1>
       <HomePage></HomePage>
+      <TestSergi></TestSergi>
       <Menu />
     </div>
   );
